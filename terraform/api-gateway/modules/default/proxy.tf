1edf89aee9f95f8fd5a4e4c617f0f1776cf08e7f
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 13:04:19 -0300

    options

--- terraform/api-gateway/modules/default/proxy.tf
+++ terraform/api-gateway/modules/default/proxy.tf
@@ -58,45 +58,45 @@ module "proxy-delete" {
   resource_id = aws_api_gateway_resource.proxy.id
 }
 
-#module "proxy-option" {
-#  source                      = "../api-gateway-resources"
-#  depends_on                  = [
-#    aws_api_gateway_resource.proxy
-#  ]
-#  rest_api_id                 = aws_api_gateway_resource.proxy.rest_api_id
-#  http_method                 = "OPTIONS"
-#  method = {
-#    authorization  = "NONE"
-#    authorizer_id   = ""
-#    request_method_api_key_required = false
-#    request_parameters = {}
-#  }
-#  integration = {
-#    uri = ""
-#    type = "MOCK"
-#    integration_http_method = "OPTIONS"
-#    request_parameters = {}
-#    request_templates = {
-#      "application/json" = "{ statusCode: 200 }"
-#    }
-#  }
-#  integration_response = {
-#    integration_response_status_code = "200"
-#    response_templates = {}
-#    response_parameters = {
-#      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,token,X-Requested-With,Cache-Control,accesstoken'",
-#      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD'",
-#      "method.response.header.Access-Control-Allow-Origin"  = "'*'",
-#    }
-#  }
-#  method_response = {
-#    status_code = "200"
-#    response_models = {}
-#    response_parameters = {
-#      "method.response.header.Access-Control-Allow-Headers" = true,
-#      "method.response.header.Access-Control-Allow-Methods" = true,
-#      "method.response.header.Access-Control-Allow-Origin" = true
-#    }
-#  }
-#  resource_id = aws_api_gateway_resource.proxy.id
-#}
+module "proxy-option" {
+  source                      = "../api-gateway-resources"
+  depends_on                  = [
+    aws_api_gateway_resource.proxy
+  ]
+  rest_api_id                 = var.api_data.rest_api_id
+  http_method                 = "OPTIONS"
+  method = {
+    authorization  = "NONE"
+    authorizer_id   = ""
+    request_method_api_key_required = false
+    request_parameters = {}
+  }
+  integration = {
+    uri = ""
+    type = "MOCK"
+    integration_http_method = "OPTIONS"
+    request_parameters = {}
+    request_templates = {
+      "application/json" = "{ statusCode: 200 }"
+    }
+  }
+  integration_response = {
+    integration_response_status_code = "200"
+    response_templates = {}
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,token,X-Requested-With,Cache-Control,accesstoken'",
+      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD'",
+      "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    }
+  }
+  method_response = {
+    status_code = "200"
+    response_models = {}
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+    }
+  }
+  resource_id = aws_api_gateway_resource.proxy.id
+}
