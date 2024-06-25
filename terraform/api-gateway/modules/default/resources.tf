e8b834cf3136868315cda0306b74521e0eab637e
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 12:07:15 -0300

    add internal docs

--- terraform/api-gateway/modules/default/resources.tf
+++ terraform/api-gateway/modules/default/resources.tf
@@ -13,9 +13,9 @@ resource "aws_api_gateway_resource" "proxy" {
   depends_on = [aws_api_gateway_resource.default]
 }
 
-#resource "aws_api_gateway_resource" "internal_docs" {
-#  for_each    = var.api_data
-#  parent_id   = data.aws_api_gateway_resource.internal_docs[each.key].id
-#  path_part   = var.path
-#  rest_api_id = each.value["rest_api_id"]
-#}
+resource "aws_api_gateway_resource" "internal_docs" {
+  for_each    = data.aws_api_gateway_resource.internal_docs
+  parent_id   = each.value.parent_id
+  path_part   = var.path
+  rest_api_id = each.value["rest_api_id"]
+}
