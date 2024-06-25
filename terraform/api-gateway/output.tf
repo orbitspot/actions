33e7aad0f81ef98707686c4d3ae40018c3ecd45d
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 11:00:48 -0300

    print values

--- terraform/api-gateway/output.tf
+++ terraform/api-gateway/output.tf
@@ -5,3 +5,7 @@ output "branch" {
 output "current_api_gateway" {
   value = local.current_api_gateway
 }
+
+output "repository_name" {
+  value = var.repository_name
+}
--- terraform/api-gateway/variable.tf
+++ terraform/api-gateway/variable.tf
@@ -1,17 +1,8 @@
-#variable "api_data" {
-#  type = list(object({
-#    parent_id = string
-#    rest_api_id = string
-#    custom_authorizer = string
-#  }))
-#}
-#
-#
-#variable "load_balancer" {
-#  type = string
-#}
-
 variable "branch" {
+  type = string
+}
+
+variable "repository_name" {
   type = string
 }
 
