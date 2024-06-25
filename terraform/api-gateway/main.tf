14631550728f583e1846417bd76d5131656ca8e5
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 11:40:13 -0300

    remove all

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -1,11 +1,11 @@
-module "api-gateway" {
-    source = "./modules/default"
-    api_data = {
-      for k, v in local.current_api_gateway : v["rest_api_id"] => v
-    }
-    load_balancer = local.uri
-    path = local.api_gateway_resource
-}
-
-
-
+#module "api-gateway" {
+#    source = "./modules/default"
+#    api_data = {
+#      for k, v in local.current_api_gateway : v["rest_api_id"] => v
+#    }
+#    load_balancer = local.uri
+#    path = local.api_gateway_resource
+#}
+#
+#
+#
