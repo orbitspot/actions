f1e4891afcdb22a11c21c53bacaedf8e01fc3ce7
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 13:05:25 -0300

    add on internal docs

--- terraform/api-gateway/modules/default/internal-docs.tf
+++ terraform/api-gateway/modules/default/internal-docs.tf
@@ -38,48 +38,48 @@ module "internal_docs_get" {
     }
     status_code = "200"
   }
-  resource_id = data.aws_api_gateway_resource.internal_docs.id
+  resource_id = aws_api_gateway_resource.internal_docs.id
 }
 
-#module "internal_docs_options" {
-#  source                      = "../api-gateway-resources"
-#  depends_on                  = [
-#    aws_api_gateway_resource.internal_docs,
-#  ]
-#  rest_api_id                 = var.api_data.rest_api_id
-#  http_method                 = "OPTIONS"
-#  method = {
-#    authorization  = "NONE"
-#    authorizer_id   = ""
-#    request_method_api_key_required = false
-#    request_parameters = {}
-#  }
-#  integration = {
-#    integration_http_method = "OPTIONS"
-#    uri = ""
-#    type = "MOCK"
-#    request_parameters = {}
-#    request_templates = {
-#      "application/json" = "{ statusCode: 200 }"
-#    }
-#  }
-#  integration_response = {
-#    integration_response_status_code = "200"
-#    response_templates = {}
-#    response_parameters = {
-#      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,token,X-Requested-With,Cache-Control,accesstoken'",
-#      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD'",
-#      "method.response.header.Access-Control-Allow-Origin"  = "'*'",
-#    }
-#  }
-#  method_response = {
-#    status_code = "200"
-#    response_models = {}
-#    response_parameters = {
-#      "method.response.header.Access-Control-Allow-Headers" = true,
-#      "method.response.header.Access-Control-Allow-Methods" = true,
-#      "method.response.header.Access-Control-Allow-Origin" = true
-#    }
-#  }
-#  resource_id = data.aws_api_gateway_resource.internal_docs.id
-#}
+module "internal_docs_options" {
+  source                      = "../api-gateway-resources"
+  depends_on                  = [
+    aws_api_gateway_resource.internal_docs,
+  ]
+  rest_api_id                 = var.api_data.rest_api_id
+  http_method                 = "OPTIONS"
+  method = {
+    authorization  = "NONE"
+    authorizer_id   = ""
+    request_method_api_key_required = false
+    request_parameters = {}
+  }
+  integration = {
+    integration_http_method = "OPTIONS"
+    uri = ""
+    type = "MOCK"
+    request_parameters = {}
+    request_templates = {
+      "application/json" = "{ statusCode: 200 }"
+    }
+  }
+  integration_response = {
+    integration_response_status_code = "200"
+    response_templates = {}
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,token,X-Requested-With,Cache-Control,accesstoken'",
+      "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,PUT,POST,DELETE,PATCH,HEAD'",
+      "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    }
+  }
+  method_response = {
+    status_code = "200"
+    response_models = {}
+    response_parameters = {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+    }
+  }
+  resource_id = aws_api_gateway_resource.internal_docs.id
+}
