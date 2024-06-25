3ee196663db475b22e5348b8fd3c41f1c36cc9de
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-06-25 12:30:01 -0300

    separando criacao

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -1,11 +1,14 @@
-#module "api-gateway" {
-#    source = "./modules/default"
-#    api_data = {
-#      for k, v in local.current_api_gateway : v["rest_api_id"] => v
-#    }
-#    load_balancer = local.uri
-#    path = local.api_gateway_resource
-#}
-#
-#
-#
+locals {
+    api_data = {
+      for k, v in local.current_api_gateway : k => v
+    }
+}
+
+module "api-gateway-first" {
+    source = "./modules/default"
+    api_data = {
+        for index, value in local.api_data[0] : value["rest_api_id"] => value
+    }
+    load_balancer = local.uri
+    path = local.api_gateway_resource
+}
