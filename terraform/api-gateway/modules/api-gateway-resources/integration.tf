1dda3f1ee591ca7f0bc96a2694b7d997689ba40b
= <=>
2024-08-12 22:04:38 -0300

    removendo http integration

--- terraform/api-gateway/modules/api-gateway-resources/integration.tf
+++ terraform/api-gateway/modules/api-gateway-resources/integration.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_integration" "default" {
       aws_api_gateway_method.default
     ]
     http_method               = aws_api_gateway_method.default.http_method
-    integration_http_method   = var.integration.integration_http_method
+    # integration_http_method   = var.integration.integration_http_method
     resource_id               = var.resource_id
     rest_api_id               = var.rest_api_id
     uri                       = var.integration.uri
@@ -11,4 +11,5 @@ resource "aws_api_gateway_integration" "default" {
     request_parameters        = var.integration.request_parameters
     request_templates         = var.integration.request_templates
     timeout_milliseconds      = var.timeout_milliseconds
+    passthrough_behavior      = "WHEN_NO_TEMPLATES"
 }
