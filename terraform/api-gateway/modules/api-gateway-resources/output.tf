b5cd8dc979f3d3dc48dd7b8e8e041cb74491fd60
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-10-01 20:12:42 -0300

    revert

--- terraform/api-gateway/modules/api-gateway-resources/output.tf
+++ terraform/api-gateway/modules/api-gateway-resources/output.tf
@@ -1,27 +1,20 @@
-#output "method" {
-#    value = aws_api_gateway_method.default
-#}
-#
-#output "integration" {
-#    value = aws_api_gateway_integration.default
-#}
-#
-#output "method_response" {
-#    value = aws_api_gateway_method_response.default
-#}
-#
-#output "integration_response" {
-#    value = aws_api_gateway_integration_response.default
-#}
-#
-#
-#
-#
-
-output "resource_id" {
-    value = var.resource_id
+output "method" {
+    value = aws_api_gateway_method.default
 }
 
-output "rest_api_id" {
-    value = var.rest_api_id
+output "integration" {
+    value = aws_api_gateway_integration.default
 }
+
+output "method_response" {
+    value = aws_api_gateway_method_response.default
+}
+
+output "integration_response" {
+    value = aws_api_gateway_integration_response.default
+}
+
+
+
+
+
