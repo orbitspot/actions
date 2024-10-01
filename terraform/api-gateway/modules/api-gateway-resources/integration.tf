2426263768ba08a3a50785fc3b654c3302aa4884
Guilherme Giusti <guilherme.giusti04@gmail.com>
2024-10-01 19:50:08 -0300

    output

--- terraform/api-gateway/modules/api-gateway-resources/integration.tf
+++ terraform/api-gateway/modules/api-gateway-resources/integration.tf
@@ -1,15 +1,15 @@
-resource "aws_api_gateway_integration" "default" {
-    depends_on                = [
-      aws_api_gateway_method.default
-    ]
-    http_method               = aws_api_gateway_method.default.http_method
-    integration_http_method   = var.integration.integration_http_method
-    resource_id               = var.resource_id
-    rest_api_id               = var.rest_api_id
-    uri                       = var.integration.uri
-    type                      = var.integration.type
-    request_parameters        = var.integration.request_parameters
-    request_templates         = var.integration.request_templates
-    timeout_milliseconds      = var.timeout_milliseconds
-    passthrough_behavior      = "WHEN_NO_TEMPLATES"
-}
+#resource "aws_api_gateway_integration" "default" {
+#    depends_on                = [
+#      aws_api_gateway_method.default
+#    ]
+#    http_method               = aws_api_gateway_method.default.http_method
+#    integration_http_method   = var.integration.integration_http_method
+#    resource_id               = var.resource_id
+#    rest_api_id               = var.rest_api_id
+#    uri                       = var.integration.uri
+#    type                      = var.integration.type
+#    request_parameters        = var.integration.request_parameters
+#    request_templates         = var.integration.request_templates
+#    timeout_milliseconds      = var.timeout_milliseconds
+#    passthrough_behavior      = "WHEN_NO_TEMPLATES"
+#}
--- terraform/api-gateway/modules/api-gateway-resources/output.tf
+++ terraform/api-gateway/modules/api-gateway-resources/output.tf
@@ -1,20 +1,27 @@
-output "method" {
-    value = aws_api_gateway_method.default
+#output "method" {
+#    value = aws_api_gateway_method.default
+#}
+#
+#output "integration" {
+#    value = aws_api_gateway_integration.default
+#}
+#
+#output "method_response" {
+#    value = aws_api_gateway_method_response.default
+#}
+#
+#output "integration_response" {
+#    value = aws_api_gateway_integration_response.default
+#}
+#
+#
+#
+#
+
+output "resource_id" {
+    value = var.resource_id
 }
 
-output "integration" {
-    value = aws_api_gateway_integration.default
+output "rest_api_id" {
+    value = var.rest_api_id
 }
-
-output "method_response" {
-    value = aws_api_gateway_method_response.default
-}
-
-output "integration_response" {
-    value = aws_api_gateway_integration_response.default
-}
-
-
-
-
-
