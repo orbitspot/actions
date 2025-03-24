7cf97b42a101b14ff5c4f902465e6c3b25845b6b
Ana Theobald <anaptheobald@gmail.com>
2025-03-24 12:32:07 -0300

    teste replace parameters

--- .github/scripts/replace-properties.py
+++ .github/scripts/replace-properties.py
@@ -1,7 +1,7 @@
 import sys
 import re
 import os
-def replace_properties_in_yaml(properties_path, yaml_path, prefix):
+def replace_properties_in_yaml(properties_path, yaml_path):
     # Carregar o arquivo properties em um dicionário
     properties = {}
     with open(properties_path, 'r') as prop_file:
@@ -20,7 +20,7 @@ def replace_properties_in_yaml(properties_path, yaml_path, prefix):
 
     # Substituir as chaves no YAML pelos valores do properties
     for key, value in properties.items():
-        placeholder = f"${{{prefix}.{key}}}"
+        placeholder = f"${{.{key}}}"
         if placeholder in yaml_content:
             print(f"Substituindo {placeholder} por {value}")
             yaml_content = re.sub(re.escape(placeholder), value, yaml_content)
@@ -34,11 +34,9 @@ def replace_properties_in_yaml(properties_path, yaml_path, prefix):
 
 properties_path = os.getenv('PROPERTIES_PATH')
 yaml_path = os.getenv('YAML_PATH')
-prefix = os.getenv('PREFIX')
 
 
 print(f"Properties: {properties_path}")
 print(f"Yaml: {yaml_path}")
-print(f"Prefix: {prefix}")
 
-replace_properties_in_yaml(properties_path, yaml_path, prefix)
+replace_properties_in_yaml(properties_path, yaml_path)
