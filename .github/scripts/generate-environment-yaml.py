bfa0e770a19f15147b6430daf3a64c5d8f34a559
Ana Theobald <anaptheobald@gmail.com>
2025-04-03 14:35:39 -0300

    ajustes migration e clean up

--- .github/scripts/generate-environment-yaml.py
+++ .github/scripts/generate-environment-yaml.py
@@ -3,15 +3,14 @@ import json
 import os
 
 def generate_yaml():
-
   environments = json.loads(os.getenv('environments'))
   secrets = json.loads(os.getenv('secrets'))
   repository_name = os.getenv('repository_name')
 
   devops_variables = ['CLUSTER_REGION', 'ENV', 'CLUSTER_NAME', 'ACCESS_KEY_CODE_ARTIFACT', 'GIT_TOKEN', 'PARAMETERS_ENCRYPT_HASH', 'SECRET_ACCESS_KEY_CODE_ARTIFACT', 
                       'API_GATEWAY', 'DEVOPS_CONFIG', '_DEVOPS_CONFIG', 'github_token', 'AWS_ACCOUNT_NUMBER', 'AWS_ROLE_NAME', 'ISTIO_HOST', 'TERRAFORM_BUCKET', '_PROPERTIES', '_POLICY_JSON']
-  result_environments = {"parameters": []}
   
+  result_environments = {"parameters": []}
   for key in environments.keys():
     if len(environments[key]) > 0 and key not in devops_variables:
       result_environments["parameters"].append({"context": "ssmparameter", "name": f'/{repository_name}/environment/{key}'}) 
