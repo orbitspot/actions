33120f3e570d5dbda42c3fb7f19edf492cf73045
Ana Theobald <anaptheobald@gmail.com>
2025-04-03 13:57:41 -0300

    alterando tag

--- .github/scripts/generate-environment-yaml.py
+++ .github/scripts/generate-environment-yaml.py
@@ -9,16 +9,16 @@ def generate_yaml():
 
   devops_variables = ['CLUSTER_REGION', 'ENV', 'CLUSTER_NAME', 'ACCESS_KEY_CODE_ARTIFACT', 'GIT_TOKEN', 'PARAMETERS_ENCRYPT_HASH', 'SECRET_ACCESS_KEY_CODE_ARTIFACT', 
                       'API_GATEWAY', 'DEVOPS_CONFIG', '_DEVOPS_CONFIG', 'github_token', 'AWS_ACCOUNT_NUMBER', 'AWS_ROLE_NAME', 'ISTIO_HOST', 'TERRAFORM_BUCKET', '_PROPERTIES', '_POLICY_JSON']
-  result_environments = []
+  result_environments = ["parameters" : []]
   print(type(environments))
   for key in environments.keys():
     if len(environments[key]) > 0 and key not in devops_variables:
-      result_environments.append({"context": "ssmparameter", "name": f'{repository_name}/environment/{key}'}) 
+      result_environments["parameters"].append({"context": "ssmparameter", "name": f'{repository_name}/environment/{key}'}) 
 
   result_secrets = []
   for key in secrets.keys():
     if len(secrets[key]) > 0 and key not in devops_variables:
-      result_secrets.append({"context": "ssmparameter", "name": f'{repository_name}/secret/{key}'}) 
+      result_environments["parameters"].append({"context": "ssmparameter", "name": f'{repository_name}/secret/{key}'}) 
 
   with open(f"variables.yaml", "w") as outfile:
     yaml.dump(result_environments, outfile, default_flow_style=False)
