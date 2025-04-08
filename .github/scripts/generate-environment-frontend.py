bfedadf2ab05d3ea509e8be661789a1f15f93a3a
Ana Theobald <anaptheobald@gmail.com>
2025-04-08 11:37:50 -0300

    add .env.production

--- .github/scripts/generate-environment-frontend.py
+++ .github/scripts/generate-environment-frontend.py
@@ -6,7 +6,7 @@ def generate_env():
   devops_variables = ['ACM_ARN', 'CLUSTER_REGION', 'ENV', 'CLUSTER_NAME', 'ACCESS_KEY_CODE_ARTIFACT', 'GIT_TOKEN', 'PARAMETERS_ENCRYPT_HASH', 'SECRET_ACCESS_KEY_CODE_ARTIFACT', 
                       'API_GATEWAY', 'DEVOPS_CONFIG', '_DEVOPS_CONFIG', 'github_token', 'AWS_ACCOUNT_NUMBER', 'AWS_ROLE_NAME', 'ISTIO_HOST', 'TERRAFORM_BUCKET', '_PROPERTIES', 'HOST_MF', '_POLICY_JSON']
 
-  with open('.env', 'w+') as env:
+  with open('.env.production', 'w+') as env:
     for key in environments:
       value = environments[key]
       if len(value) > 0 and key not in devops_variables: 
