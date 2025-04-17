31e86a71f5974f6c476a285f78bd514443115021
Ana Theobald <anaptheobald@gmail.com>
2025-04-17 11:25:32 -0300

    ajustes variaveis que começam com _

--- .github/scripts/generate-environment-yaml.py
+++ .github/scripts/generate-environment-yaml.py
@@ -7,12 +7,12 @@ def generate_yaml():
   secrets = json.loads(os.getenv('secrets'))
   repository_name = os.getenv('repository_name')
 
-  devops_variables = ['CLUSTER_REGION', 'ENV', 'CLUSTER_NAME', 'ACCESS_KEY_CODE_ARTIFACT', 'GIT_TOKEN', 'PARAMETERS_ENCRYPT_HASH', 'SECRET_ACCESS_KEY_CODE_ARTIFACT', '_FRONTEND_CONFIG', 'AWS_REGION',
-                      'API_GATEWAY', 'DEVOPS_CONFIG', '_DEVOPS_CONFIG', 'github_token', 'AWS_ACCOUNT_NUMBER', 'AWS_ROLE_NAME', 'ISTIO_HOST', 'TERRAFORM_BUCKET', '_PROPERTIES', '_POLICY_JSON', 'API_HOST']
+  devops_variables = ['CLUSTER_REGION', 'ENV', 'CLUSTER_NAME', 'ACCESS_KEY_CODE_ARTIFACT', 'GIT_TOKEN', 'PARAMETERS_ENCRYPT_HASH', 'SECRET_ACCESS_KEY_CODE_ARTIFACT', 'AWS_REGION',
+                              'API_GATEWAY', 'DEVOPS_CONFIG', 'github_token', 'AWS_ACCOUNT_NUMBER', 'AWS_ROLE_NAME', 'ISTIO_HOST', 'TERRAFORM_BUCKET', 'API_HOST']
   
   result_environments = {"parameters": []}
   for key in environments.keys():
-    if len(environments[key]) > 0 and key not in devops_variables:
+    if len(environments[key]) > 0 and key not in devops_variables and not key.startswith("_"):
       result_environments["parameters"].append({"context": "ssmparameter", "name": f'/{repository_name}/environment/{key}'}) 
 
   result_secrets = []
