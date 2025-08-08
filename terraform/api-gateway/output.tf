880cd9f27df553503b3b483587f83a6639e46892
Christian <31663828+cantarino@users.noreply.github.com>
2025-08-08 16:52:49 -0300

    chore: unused output

--- terraform/api-gateway/output.tf
+++ terraform/api-gateway/output.tf
@@ -27,13 +27,13 @@ output "path" {
   value = local.api_gateway_resource
 }
 
-output "first_api" {
-  value = local.api_data[0]
-}
+# output "first_api" {
+#   value = local.api_data[0]
+# }
 
-output "second_api" {
-  value = local.api_data[1]
-}
+# output "second_api" {
+#   value = local.api_data[1]
+# }
 
 # output "module" {
 #   value = module.api-gateway-first
