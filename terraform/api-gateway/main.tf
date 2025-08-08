b15a5a0af9d32cf9a6d1f9454d656be4df9beebd
Christian <31663828+cantarino@users.noreply.github.com>
2025-08-08 16:11:52 -0300

    feat: add oauth routes in api-gateway

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -1,28 +1,42 @@
-locals {
-    api_data = {
-      for k, v in local.current_api_gateway : k => v
-    }
-}
+# locals {
+#   api_data = {
+#     for k, v in local.current_api_gateway : k => v
+#   }
+# }
 
-module "api-gateway-first" {
-    source = "./modules/default"
-    api_data = local.current_api_gateway[0]
-    load_balancer = local.uri
-    path = local.api_gateway_resource
-    istio_enabled = var.istio_enabled
-    docs = var.docs
-    resource_name = var.resource_name
-    apply_response_script = var.apply_response_script
-}
+# module "api-gateway-first" {
+#   source                = "./modules/default"
+#   api_data              = local.current_api_gateway[0]
+#   load_balancer         = local.uri
+#   path                  = local.api_gateway_resource
+#   istio_enabled         = var.istio_enabled
+#   docs                  = var.docs
+#   resource_name         = var.resource_name
+#   apply_response_script = var.apply_response_script
+# }
 
-module "api-gateway-second" {
-    source = "./modules/default"
-    api_data = local.current_api_gateway[1]
-    load_balancer = local.uri
-    path = local.api_gateway_resource
-    istio_enabled = var.istio_enabled
-    docs = var.docs
-    resource_name = var.resource_name
-    apply_response_script = var.apply_response_script
-}
+# module "api-gateway-second" {
+#   source                = "./modules/default"
+#   api_data              = local.current_api_gateway[1]
+#   load_balancer         = local.uri
+#   path                  = local.api_gateway_resource
+#   istio_enabled         = var.istio_enabled
+#   docs                  = var.docs
+#   resource_name         = var.resource_name
+#   apply_response_script = var.apply_response_script
+# }
 
+module "api-gateway-oauth2-second" {
+  source = "./modules/default"
+  api_data = object({
+    parent_id         = "h0ebgzn072"
+    rest_api_id       = "d4c33alv35"
+    custom_authorizer = "dhz1f6"
+  })
+  load_balancer         = local.uri
+  path                  = local.api_gateway_resource
+  istio_enabled         = var.istio_enabled
+  docs                  = var.docs
+  resource_name         = var.resource_name
+  apply_response_script = var.apply_response_script
+}
