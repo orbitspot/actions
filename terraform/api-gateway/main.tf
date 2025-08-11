6028802fb17fab281bfead6e4bc6fa1adc04666d
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2025-08-11 10:01:10 -0300

    chore: remove unused

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -4,46 +4,19 @@ locals {
   }
 }
 
-# module "api-gateway-oauth2-second" {
-#   source = "./modules/default"
-#   api_data = {
-#     parent_id         = "gqh1ab"
-#     rest_api_id       = "d4c33alv35"
-#     custom_authorizer = "dhz1f6"
-#   }
-#   load_balancer         = local.uri
-#   path                  = local.api_gateway_resource
-#   istio_enabled         = var.istio_enabled
-#   docs                  = var.docs
-#   resource_name         = var.resource_name
-#   apply_response_script = var.apply_response_script
-# }
-
 module "api_gateway" {
-  for_each = local.api_data
   source   = "./modules/default"
-  api_data = {
-    parent_id         = each.value["parent_id"]
-    rest_api_id       = each.value["rest_api_id"]
-    custom_authorizer = each.value["custom_authorizer"]
-  }
-  # api_data              = each.value
+  for_each = local.api_data
+  api_data              = each.value
   load_balancer         = local.uri
   path                  = local.api_gateway_resource
   istio_enabled         = var.istio_enabled
-  docs                  = var.docs
-  resource_name         = var.resource_name
   apply_response_script = var.apply_response_script
 }
 
 module "internal_docs" {
-  for_each = local.api_data
   source   = "./modules/internal-docs"
-  # api_data = {
-  #   parent_id         = each.value["parent_id"]
-  #   rest_api_id       = each.value["rest_api_id"]
-  #   custom_authorizer = each.value["custom_authorizer"]
-  # }
+  for_each = local.api_data
   api_data      = each.value
   load_balancer = local.uri
   path          = local.api_gateway_resource
