a79d37b842236bbebda0e0a704b5f6c13967dd54
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2025-08-11 10:25:33 -0300

    feat: add oauth routes

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -2,11 +2,25 @@ locals {
   api_data = {
     for index, obj in local.current_api_gateway : obj.rest_api_id => obj
   }
+
+  api_oauth_data = {
+    for index, obj in local.current_api_gateway_oauth : obj.rest_api_id => obj
+  }
 }
 
-module "api_gateway" {
+module "default_routes" {
   source   = "./modules/default"
   for_each = local.api_data
+  api_data              = each.value
+  load_balancer         = local.uri
+  path                  = local.api_gateway_resource
+  istio_enabled         = var.istio_enabled
+  apply_response_script = var.apply_response_script
+}
+
+module "oauth_routes" {
+  source   = "./modules/default"
+  for_each = local.api_oauth_data
   api_data              = each.value
   load_balancer         = local.uri
   path                  = local.api_gateway_resource
--- terraform/api-gateway/variable.tf
+++ terraform/api-gateway/variable.tf
@@ -61,3 +61,43 @@ variable "apply_response_script" {
   type = bool
   default = true
 }
+
+variable "api_gateway_oauth2" {
+  type = object({
+    master = list(object({
+      parent_id = string
+      rest_api_id = string
+      custom_authorizer = string
+      region = string
+    }))
+    homolog = list(object({
+      parent_id = string
+      rest_api_id = string
+      custom_authorizer = string
+      region = string
+    }))
+    develop = list(object({
+      parent_id = string
+      rest_api_id = string
+      custom_authorizer = string
+      region = string
+    }))
+  })
+  # TODO: Remove
+  default = {
+    develop = [ 
+      {
+       parent_id: "gqh1ab",
+        rest_api_id: "d4c33alv35",
+        custom_authorizer: "dhz1f6",
+        region: "us-east-1"
+    },
+    {
+       parent_id: "dc8sdh",
+        rest_api_id: "vvu27u8aga",
+        custom_authorizer: "rive7o",
+        region: "us-east-1"
+    },
+     ]
+  }
+}
