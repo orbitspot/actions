0580f1add4901eb7c4a6dc03d49b814f512eddec
Christian <31663828+cantarino@users.noreply.github.com>
2025-11-10 18:13:48 -0300

    chore: comment default

--- terraform/api-gateway/modules/api-gateway-resources/output.tf
+++ terraform/api-gateway/modules/api-gateway-resources/output.tf
@@ -1,18 +1,18 @@
 output "method" {
-    value = aws_api_gateway_method.default
+  value = aws_api_gateway_method.default
 }
 
 output "integration" {
-    value = aws_api_gateway_integration.default
+  value = aws_api_gateway_integration.default
 }
 
 output "method_response" {
-    value = aws_api_gateway_method_response.default
+  value = aws_api_gateway_method_response.default
 }
 
-output "integration_response" {
-    value = aws_api_gateway_integration_response.default
-}
+# output "integration_response" {
+#     value = aws_api_gateway_integration_response.default
+# }
 
 
 
