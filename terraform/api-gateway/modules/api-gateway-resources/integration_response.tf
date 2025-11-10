c28c5d354de1c0bbd1dc5b8b68cb153518f35bf2
Christian <31663828+cantarino@users.noreply.github.com>
2025-11-10 17:36:41 -0300

    chore: creating integrations in order

--- terraform/api-gateway/modules/api-gateway-resources/integration_response.tf
+++ terraform/api-gateway/modules/api-gateway-resources/integration_response.tf
@@ -1,12 +1,107 @@
-resource "aws_api_gateway_integration_response" "default" {
-    for_each                  = var.integration_response.status_code
-    depends_on                = [ aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default ]
-    http_method               = aws_api_gateway_method.default.http_method
-    resource_id               = var.resource_id
-    rest_api_id               = var.rest_api_id
-    status_code               = each.key
-    selection_pattern         = each.value
-    response_templates        = var.integration_response.response_templates
-    response_parameters       = var.integration_response.response_parameters
-    region                    = "us-east-1"
+resource "aws_api_gateway_integration_response" "integration_201" {
+  depends_on          = [aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "201"
+  selection_pattern   = "201"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "integration_204" {
+  depends_on          = [aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "204"
+  selection_pattern   = "204"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "integration_403" {
+  depends_on          = [aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "403"
+  selection_pattern   = "403"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "integration_404" {
+  depends_on          = [aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "404"
+  selection_pattern   = "404"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "integration_400" {
+  depends_on = [
+    aws_api_gateway_integration.default,
+    aws_api_gateway_method.default,
+    aws_api_gateway_method_response.default,
+    aws_api_gateway_integration_response.inregration_201,
+    aws_api_gateway_integration_response.inregration_204,
+    aws_api_gateway_integration_response.integration_403,
+    aws_api_gateway_integration_response.integration_404
+  ]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "400"
+  selection_pattern   = "4.*"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "inregration_500" {
+  depends_on = [
+    aws_api_gateway_integration.default,
+    aws_api_gateway_method.default,
+    aws_api_gateway_method_response.default,
+    aws_api_gateway_integration_response.inregration_201,
+    aws_api_gateway_integration_response.inregration_204,
+    aws_api_gateway_integration_response.integration_403,
+    aws_api_gateway_integration_response.integration_404
+  ]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "500"
+  selection_pattern   = "5.*"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
+}
+
+resource "aws_api_gateway_integration_response" "inregration_200" {
+  depends_on = [
+    aws_api_gateway_integration.default,
+    aws_api_gateway_method.default,
+    aws_api_gateway_method_response.default,
+    aws_api_gateway_integration_response.inregration_201,
+    aws_api_gateway_integration_response.inregration_204,
+    aws_api_gateway_integration_response.integration_403,
+    aws_api_gateway_integration_response.integration_404
+  ]
+  http_method         = aws_api_gateway_method.default.http_method
+  resource_id         = var.resource_id
+  rest_api_id         = var.rest_api_id
+  status_code         = "200"
+  selection_pattern   = "20[0-35-9]"
+  response_templates  = var.integration_response.response_templates
+  response_parameters = var.integration_response.response_parameters
+  region              = "us-east-1"
 }
