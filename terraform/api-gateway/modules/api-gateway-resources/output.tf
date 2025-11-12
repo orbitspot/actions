da0871c468ddf99a8977d43de5f48af2b13bc4d1
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2025-11-12 09:53:58 -0300

    Chore/api gateway (#76)
    * chore: fix error codes
    
    * chore: using chore tag
    
    * chore: creating integrations in order
    
    * chore: delete before
    
    * chore: delete before
    
    * chore: dont delete
    
    * chore: comment default
    
    * chore: fix typos
    
    * chore: rollback integration response and unify 400
    
    * chore: rollback tag
    
    * chore: uncoment output

--- terraform/api-gateway/modules/api-gateway-resources/output.tf
+++ terraform/api-gateway/modules/api-gateway-resources/output.tf
@@ -1,20 +1,17 @@
 output "method" {
-    value = aws_api_gateway_method.default
+  value = aws_api_gateway_method.default
 }
 
 output "integration" {
-    value = aws_api_gateway_integration.default
+  value = aws_api_gateway_integration.default
 }
 
 output "method_response" {
-    value = aws_api_gateway_method_response.default
+  value = aws_api_gateway_method_response.default
 }
 
 output "integration_response" {
-    value = aws_api_gateway_integration_response.default
+  value = aws_api_gateway_integration_response.default
 }
-
-
-
 
 
