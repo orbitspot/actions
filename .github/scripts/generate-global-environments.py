9dbd8c63c32c52655336af80e876cae2c2e5c27f
Ana Theobald <anaptheobald@gmail.com>
2025-12-09 17:39:37 -0300

    add global variable pipe

---
+++ .github/scripts/generate-global-environments.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+variables = os.getenv("variables_json", "{}")
+module = os.getenv("module")
+
+result_json = {}
+for key in variables:
+  value = variables[key].replace("$", "$$") # no terraform $$ é o escapamento para o $ 
+  print(key.split("_")[1])
+  if len(value) > 0 and key.split("_")[1] == module.upper(): # adiciona apenas as variáveis do módulo selecionado
+    result_json.update({key: value}) 
+
+with open(f"${{ inputs.file_name }}.json", "w") as outfile:
+  json.dump(result_json, outfile)
--- terraform/parameter-store/mail.tf
+++ terraform/parameter-store/mail.tf
@@ -2,14 +2,13 @@ resource "aws_ssm_parameter" "vars" {
  for_each    = {
    for index, value in local.variables : index => value
  }
- name        = "/${var.repository}/environment/${each.key}"
+ name        = var.global ? "/${var.modulo}/environment/${each.key}" : "/${var.repository}/environment/${each.key}"
  description = substr(format("Environment %s from module %s", jsonencode(each.value), var.modulo), 0, 1023)
  type        = "String"
  value       = trimspace(each.value)
  tier        = "Standard"
  overwrite   = true # na proxima versão será removido, mas usar ele por enquanto
  tags = {
-   Version = var.versionament
    "orbit:modulo": var.modulo
  }
 }
@@ -18,14 +17,13 @@ resource "aws_ssm_parameter" "scrts" {
  for_each    = {
    for index, value in local.secrets : index => value
  }
- name        = "/${var.repository}/secret/${each.key}"
+ name        = var.global ? "/${var.modulo}/secret/${each.key}" : "/${var.repository}/secret/${each.key}"
  description = substr(format("Secret %s from module %s", jsonencode(each.key), var.modulo), 0, 1023)
  type        = "SecureString"
  value       = trimspace(replace(each.value, "$$", "$"))
  tier        = "Standard"
  overwrite   = true # na proxima versão será removido, mas usar ele por enquanto
  tags = {
-   Version = var.versionament
    "orbit:modulo": var.modulo
  }
 }
