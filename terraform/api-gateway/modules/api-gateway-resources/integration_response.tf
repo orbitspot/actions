c7ca16b6bc4d9061c134b4aa62f7b40b5e401418
Ana Theobald <anaptheobald@gmail.com>
2026-01-12 11:06:55 -0300

    fix depends on

--- terraform/api-gateway/modules/api-gateway-resources/integration_response.tf
+++ terraform/api-gateway/modules/api-gateway-resources/integration_response.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_integration_response" "default" {
   for_each            = var.integration_response.status_code
-  depends_on          = [aws_api_gateway_integration.default, aws_api_gateway_method.default, aws_api_gateway_method_response.default]
+  depends_on          = [aws_api_gateway_method_response.default]
   http_method         = aws_api_gateway_method.default.http_method
   resource_id         = var.resource_id
   rest_api_id         = var.rest_api_id
--- terraform/api-gateway/modules/api-gateway-resources/methods_response.tf
+++ terraform/api-gateway/modules/api-gateway-resources/methods_response.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_method_response" "default" {
   for_each            = var.method_response.status_code
-  depends_on          = [aws_api_gateway_method.default]
+  depends_on          = [aws_api_gateway_integration.default]
   http_method         = aws_api_gateway_method.default.http_method
   resource_id         = var.resource_id
   rest_api_id         = var.rest_api_id
